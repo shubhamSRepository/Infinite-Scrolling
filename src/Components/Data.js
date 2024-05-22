58dde8f0b67c874c9a75d9aae67d4632509d7a1b
Shubham Soni <soni.shubham927@gmail.comgit config --global user.email soni.shubham927@gmail.com>
2024-05-22 15:45:29 +0530

    add infinite scrolling

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,21 +1,51 @@
 import { useEffect, useState } from 'react';
 
+
 function Data() {
 
     const [items, setItems] = useState([]);
+    const [page, setPage] = useState(1);
+
 
     const getData = async () => {
 
-        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=9&_page=1`);
+        const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=9&_page=${page}`);
 
         const data = await res.json();
-        setItems(data);
+        setItems((prev) => [...prev, ...data]);
         console.log(data);
     }
 
+
+
+
     useEffect(() => {
         getData();
+    }, [page]);
+
+
+
+    const handleInfiniteScroll = async () => {
+
+        try {
+            if (window.innerHeight + document.documentElement.scrollTop + 1 >= document.documentElement.scrollHeight) {
+                setPage((prev) => prev + 1);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+
+
+    useEffect(() => {
+
+        window.addEventListener("scroll", handleInfiniteScroll);
+        return () => window.removeEventListener("scroll", handleInfiniteScroll);
+
     }, []);
+
+
 
     return (
         <>
@@ -23,11 +53,9 @@ function Data() {
 
             <div className='container'>
 
-
-
                 {
-                    items.map((item) => (
-                        <div className='box' key={item.id}>
+                    items.map((item, index) => (
+                        <div className='box' key={index}>
                             <p>{item.id}</p>
                             <p>{item.body}</p>
                         </div>
@@ -38,4 +66,4 @@ function Data() {
     )
 }
 
-export default Data
+export default Data;
